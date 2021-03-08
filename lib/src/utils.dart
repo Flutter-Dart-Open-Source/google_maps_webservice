98dc0bd451abb041a17f3efd254d5b983cbdb923
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-03-08 10:35:46 +0700

    bump deps

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,8 +1,9 @@
 library google_maps_webservice.utils;
 
 import 'dart:async';
-import 'package:meta/meta.dart';
+
 import 'package:http/http.dart';
+import 'package:meta/meta.dart';
 
 const kGMapsUrl = 'https://maps.googleapis.com/maps/api';
 
@@ -58,7 +59,7 @@ abstract class GoogleWebService {
 
   @protected
   Future<Response> doGet(String url, {Map<String, dynamic> headers}) {
-    return httpClient.get(url, headers: headers);
+    return httpClient.get(Uri.parse(url), headers: headers);
   }
 
   @protected
@@ -71,7 +72,7 @@ abstract class GoogleWebService {
       'Content-type': 'application/json',
     };
     if (headers != null) postHeaders.addAll(headers);
-    return httpClient.post(url, body: body, headers: postHeaders);
+    return httpClient.post(Uri.parse(url), body: body, headers: postHeaders);
   }
 }
 
